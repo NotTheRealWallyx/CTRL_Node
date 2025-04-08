560a535bc3dbd12f5385147a8413ffd9d665657d
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-04-08 23:25:10 +0200

    add missing method to run pyfiglet

--- ctrl_node/variables/logos.py
+++ ctrl_node/variables/logos.py
@@ -1,4 +1,8 @@
 """ Variables that contain the logo ASCII art """
+import pyfiglet
+
+def get_logo(text):
+    return pyfiglet.figlet_format(text)
 
 SERVER_TOOLS_LOGO = get_logo("CTRL_Node")
 SCAN_PORTS_LOGO = get_logo("Scan Ports")
