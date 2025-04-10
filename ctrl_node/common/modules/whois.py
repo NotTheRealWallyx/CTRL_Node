0347350b3a131714cb4ed4c6217b62e701e7653b
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-04-10 22:25:30 +0200

    fix who is not asking for domain input

--- ctrl_node/common/modules/whois.py
+++ ctrl_node/common/modules/whois.py
@@ -1,22 +1,26 @@
 import questionary
 import whois
 
+from ctrl_node.common.utils import askforhost
+
 
 class Whois:
-    def __init__(self):
+
+    def __init__(self, host=None):
         """
         Initializes the Whois class.
         """
         self.domain_info = None
+        self.host = host or askforhost()
 
-    def fetch_domain_info(self, domain):
+    def fetch_domain_info(self):
         """
         Fetches the Whois information for the given domain.
 
         Args:
             domain {str}: The domain name to query.
         """
-        self.domain_info = whois.whois(domain)
+        self.domain_info = whois.whois(self.host)
 
     def display_domain_info(self):
         """
