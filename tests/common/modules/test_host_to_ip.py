e4c5daf07c4a0662828e39d1e38ea8dd8a109528
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-04-10 14:30:24 +0200

    update test after adding questionary intead of input

--- tests/common/modules/test_host_to_ip.py
+++ tests/common/modules/test_host_to_ip.py
@@ -10,17 +10,23 @@ class TestHostToIp(unittest.TestCase):
         self.assertEqual(instance.host, "example.com")
         mock_askforhost.assert_called_once()
 
+    @patch("ctrl_node.common.modules.host_to_ip.questionary.text")
     @patch(
         "ctrl_node.common.modules.host_to_ip.socket.gethostbyname",
         return_value="93.184.216.34",
     )
     @patch("builtins.print")
-    def test_get_ip_from_hostname(self, mock_print, mock_gethostbyname):
+    def test_get_ip_from_hostname(
+        self, mock_print, mock_gethostbyname, mock_questionary_text
+    ):
+        mock_questionary_text.return_value.ask.return_value = None
         instance = HostToIp(host="example.com")
-        with patch.object(instance, "complete", return_value=None):
-            instance.get_ip_from_hostname()
-            mock_gethostbyname.assert_called_once_with("example.com")
-            mock_print.assert_any_call("The IP for example.com is 93.184.216.34")
+        instance.get_ip_from_hostname()
+        mock_gethostbyname.assert_called_once_with("example.com")
+        mock_print.assert_any_call("The IP for example.com is 93.184.216.34")
+        mock_questionary_text.assert_called_once_with(
+            "Press Enter to return to the menu..."
+        )
 
 
 if __name__ == "__main__":
